483df8803f831ac6872aeb7d596eafb64aa90564
lamia-alam <90729375+lamia-alam@users.noreply.github.com>
2024-09-23 19:21:54 -0400

    Update hello.py -> added emailfield so error message appears like screenshot

--- hello.py
+++ hello.py
@@ -2,8 +2,8 @@ from flask import Flask, render_template, session, redirect, url_for, flash
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
-from wtforms.validators import DataRequired, Email
+from wtforms import StringField, SubmitField, EmailField
+from wtforms.validators import DataRequired, Email, InputRequired
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'hard to guess string'
@@ -13,9 +13,8 @@ moment = Moment(app)
 
 class NameForm(FlaskForm):
     name = StringField('What is your name?', validators=[DataRequired()])
-    email = StringField('What is your UofT Email address?', 
-                    validators=[DataRequired(message="Email is required"), 
-                                Email(message="Please include an '@' in your email address.")])
+    email = EmailField('What is your UofT Email address?', 
+                    validators=[DataRequired(), Email()])
 
     submit = SubmitField('Submit')
 
@@ -30,15 +29,21 @@ def internal_server_error(e):
 @app.route('/', methods=['GET', 'POST'])
 def index():
     form = NameForm()
+    if form.email.errors:
+        flash(f"Please include an '@' in the email address. '{form.email.data}' is missing an '@'.", 'danger')
     if form.validate_on_submit():
         old_name = session.get('name')
         old_email = session.get('email')
         
+        
+       
         if old_name is not None and old_name != form.name.data:
             flash('Looks like you have changed your name!')
         if old_email is not None and old_email != form.email.data:
             flash('Looks like you have changed your email!')
         
+
+
         session['name'] = form.name.data
         session['email'] = form.email.data
         
